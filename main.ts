f1373d891ac9108008651bd7e4897eb2a4e52c15
hashrock <hashrock@users.noreply.github.com>
2022-10-04 22:24:53 +0900

    update blog

--- main.ts
+++ main.ts
@@ -1,21 +1,7 @@
-import blog, { ga, redirects } from "https://deno.land/x/blog@0.1.0/blog.tsx";
+import blog from "https://deno.land/x/blog@0.5.0/blog.tsx";
 
 blog({
   title: "Hashrock's bathtub",
   author: "hashrock",
-  header: "This is my diary, but in English",
   style: "body { padding: 32px 0; background-color: #f0f0f0; }",
-
-  // middlewares: [
-    
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
-
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
-
-  // ]
 });
